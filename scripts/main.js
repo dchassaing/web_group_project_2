c6ca66681d5d1368840e15882cdff1919255aead
dchassaing <delia.chassaing@gmail.com>
2017-08-02 14:17:03 -0500

    played around with React on the .js file and added fields in the custom order form

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,49 @@
 //make the nav bar appear on every page
+
+//toggle between cakes and other
+//direct DOM manipulation using jQuery
+
+$('.show-btn').on('click', function() {
+  $('.other').show();
+});
+
+$('.hide-btn').on('click', function() {
+  $('.other').hide();
+});
+
+//indirect DOM manipulation using React
+
+class CakeBox extends React.Component {
+
+constructor() {
+  super();
+
+  this.state = {
+    showCakes: false
+  };
+}
+
+
+render() {
+  const cakes = this._getCakes();
+  let cakeNodes;
+  let buttonText = 'Show cakes';
+
+  if (this.state.showCakes) {
+    buttonText = 'Hide cakes' ;
+    cakeNodes = <div className="cake-list">{cakes}</div>;
+  }
+  return(
+    <div className="other">
+    <h3 className="h3">{this._getCakesTitle(cakes.legth)}</h3>
+    <button onClick={this._handleClick.bind(this)}>{buttonText}</button>
+
+  );
+}
+
+_handleClick(){
+  this.setState({
+    showCakes: !this.state.showCakes
+  });
+}
+}
